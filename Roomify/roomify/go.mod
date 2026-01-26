df73e0ec493b1126edc291d2ab0717e098f7559f
farfan02 <maulanafarfan18@gmail.com>
2026-01-26 21:50:31 +0700

    buat new project golang

---
+++ Roomify/roomify/go.mod
@@ -0,0 +1,3 @@
+module github.com/SKARIGA-RPL-XII/project-kik-farfan02/Roomify/roomify
+
+go 1.24.1
---
+++ Roomify/roomify/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Roomify Golang backend running 🚀")
+}
